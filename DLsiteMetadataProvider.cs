b88a41e5b4b0f5d9891f2d52cb89390f491fb329
Mysterken <Mysterkenzo@gmail.com>
2024-05-05 11:50:24 +0200

    fix null assignment

--- DLsiteMetadataProvider.cs
+++ DLsiteMetadataProvider.cs
@@ -9,22 +9,21 @@ public class DLsiteMetadataProvider(MetadataRequestOptions options, DLsiteMetada
     private readonly DLsiteMetadataPlugin _plugin = plugin;
 
     private List<MetadataField> _availableFields;
-    private GameData _gameData;
 
-    public override List<MetadataField> AvailableFields =>
-        _availableFields ??= GetAvailableFields();
+    private GameData _gameData;
+    public override List<MetadataField> AvailableFields => _availableFields ??= GetAvailableFields();
 
     // Override additional methods based on supported metadata fields.
     public override string GetDescription(GetMetadataFieldArgs args)
     {
-        if (!_availableFields.Contains(MetadataField.Description)) return base.GetDescription(args);
+        if (!AvailableFields.Contains(MetadataField.Description)) return base.GetDescription(args);
 
         return options.GameData.Name + " description";
     }
 
     public override string GetName(GetMetadataFieldArgs args)
     {
-        if (!_availableFields.Contains(MetadataField.Name)) return base.GetName(args);
+        if (!AvailableFields.Contains(MetadataField.Name)) return base.GetName(args);
 
         return options.GameData.Name;
     }
