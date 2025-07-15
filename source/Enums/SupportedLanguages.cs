76ba26d3920823cb33c66ffed64f8230437914cb
Mysterken <mysterken.dev@gmail.com>
2025-07-15 17:47:14 +0200

    refactor SupportedLanguages enum and rename file to TranslationDictionary

---
+++ source/Enums/SupportedLanguages.cs
@@ -0,0 +1,21 @@
+﻿// ReSharper disable InconsistentNaming
+
+namespace DLsiteMetadata.Enums;
+
+public enum SupportedLanguages
+{
+    ja_JP,
+    en_US,
+    zh_CN,
+    zh_TW,
+    ko_KR,
+    es_ES,
+    de_DE,
+    fr_FR,
+    id_ID,
+    it_IT,
+    pt_BR,
+    sv_SE,
+    th_TH,
+    vi_VN
+}
--- source/TranslationDictionary.cs
+++ source/TranslationDictionary.cs
@@ -1,26 +1,7 @@
-﻿// ReSharper disable InconsistentNaming
-
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using DLsiteMetadata.Enums;
 
 namespace DLsiteMetadata;
-
-public enum SupportedLanguages
-{
-    ja_JP,
-    en_US,
-    zh_CN,
-    zh_TW,
-    ko_KR,
-    es_ES,
-    de_DE,
-    fr_FR,
-    id_ID,
-    it_IT,
-    pt_BR,
-    sv_SE,
-    th_TH,
-    vi_VN
-}
 
 public static class TranslationDictionary
 {
